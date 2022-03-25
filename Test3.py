3e3a66df75336c162154c169de8f02b5573c34ec
Hasin Ishrak <mohammed.hasin.ishrak@g.bracu.ac.bd>
2022-03-25 10:35:25 +0900

    Now able to wrtie on file

--- Test3.py
+++ Test3.py
@@ -4,9 +4,20 @@ import csv
 import yfinance as yf
 f = open("myfile.txt", "w")
 
+#Definations
+
+def PrintDataFrame(dataframe,path_to_print): 
+    Path=path_to_print
+    df=dataframe
+    df.to_csv(path_or_buf=Path)
+
 yf.pdr_override() # <== that's all it takes :-)
 
 # download dataframe using pandas_datareader
 data = pdr.get_data_yahoo("SPY", start="2022-01-01", end="2022-01-30")
-df.to_csv("C:\Users\hasin\OneDrive\Desktop\Projects\yfinance\CSV.txt")
+print(data)
+print(type(data))
+PrintDataFrame(data,'Test2_CSV.csv')
+
+
 
